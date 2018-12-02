cb06334fbd1e31bb8d644902947ca8d5902ae1a1
Ruzal Shrestha <shrestharujal1928@gmail.com>
2018-12-02 11:52:41 +0545

    templates

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(response):
-    return HttpResponse("Hello World!")
+def index(request):
+    my_dict = {'my_name': "Rujal Shrestha"}
+    return render(request,"first_app/index.html",context=my_dict)
---
+++ templates/README.md
@@ -0,0 +1,2 @@
+#firstly after we create "templates" directory which contains html, js and css.<br/>
+#because we created this directory we need to let know "setting.py". we do this by adding directory under "TEMPLATES" -> "DIRS"
